2260420571c73f7405ecc2f5b9a720bca3b63989
qrumii <rizqi.jbi@gmail.com>
2025-07-04 20:32:51 +0700

    MBC Landing Page

--- app/kontak/page.tsx
+++ app/kontak/page.tsx
@@ -16,7 +16,7 @@ export default function Kontak() {
     const data = { nama, email, pesan };
 
     try {
-      const res = await fetch("http://localhost:5000/api/contact", {
+      const res = await fetch("https://mbc-laboratory.onrender.com/api/contact", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
